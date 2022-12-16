3fa15926a18b8d79207aad3745114e4a4366a638
ALVINdimpos <alvindimpos@gmail.com>
2022-12-16 14:47:49 +0200

    footer link changed

--- src/components/footer/Index.js
+++ src/components/footer/Index.js
@@ -65,15 +65,12 @@ const Index = () => {
             </div>
             <ul>
               <li><a href="#">Home</a></li>
-              <li><a href="#">about</a></li>
-              <li><a href="#">services</a></li>
-              <li><a href="#">portfolio</a></li>
-              <li><a href="#">Contact</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Blogs</a></li>
               <li><a href="#">About us</a></li>
               <li><a href="#">Our Services</a></li>
-              <li><a href="#">Expert Team</a></li>
               <li><a href="#">Contact us</a></li>
-              <li><a href="#">Latest News</a></li>
+            
             </ul>
           </div>
         </div>
