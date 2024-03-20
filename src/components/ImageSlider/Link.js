5a74e9005ff935ded3f9528d509649c18de8f5a5
Moise NIZEYIMANA <59395205+KNMoise@users.noreply.github.com>
2024-03-20 09:15:37 +0200

    Update Link.js
    Paris image commented

--- src/components/ImageSlider/Link.js
+++ src/components/ImageSlider/Link.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css'
 import Slider1 from '../../assets/lelo.png';
-import Slider2 from '../../assets/paris.png';
+// import Slider2 from '../../assets/paris.png';
 import Slider3 from '../../assets/park.png';
 import './Slide.css'
 const divStyle = {
