88fada4dca9451a55d76e70c1763e62141495d7e
Moise NIZEYIMANA <59395205+KNMoise@users.noreply.github.com>
2024-06-14 13:26:10 +0200

    Update Link.js

--- src/components/ImageSlider/Link.js
+++ src/components/ImageSlider/Link.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css'
-import Slider1 from '../../assets/Magazine.png';
+import Slider1 from '../../assets/tronics.png';
 import Slider2 from '../../assets/kigalirent.png';
 import Slider3 from '../../assets/park.png';
 import './Slide.css'
