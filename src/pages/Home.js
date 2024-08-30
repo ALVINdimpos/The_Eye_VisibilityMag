352f47d4e35d80a4ee1affb87faca39e7f58d630
Moise NIZEYIMANA <59395205+KNMoise@users.noreply.github.com>
2024-08-30 05:35:01 -0700

    Update Home.js
    Add the video branding file to the assets folder: Make sure the video branding file

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,6 +10,7 @@ import IMAG4 from "../assets/axar.jpg";
 import IMAG5 from "../assets/Maga-A4.jpg";
 import Card from "../components/cards/Index";
 import kglCarRental from "../assets/kglCarRental.jpeg";
+import BrandingVideo from "../assets/Branding.mp4";
 import "./Home.css";
 import { BsFillArrowRightCircleFill } from "react-icons/bs";
 const HomePage = () => {
@@ -28,6 +29,11 @@ const HomePage = () => {
         <h3>Latest Videos</h3>
       </div>
       <div className="video_wrapper">
+        {/* Video Branding */}
+        <video width="410" height="371" controls>
+          <source src={BrandingVideo} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
         <iframe
           width="410"
           height="371"
