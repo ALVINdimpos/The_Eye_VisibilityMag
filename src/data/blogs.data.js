4a2ab0fd9a61c29bcc2e8bac97cadb7861fb3b37
Moise NIZEYIMANA <59395205+KNMoise@users.noreply.github.com>
2024-09-20 05:12:25 -0700

    Update blogs.data.js

--- src/data/blogs.data.js
+++ src/data/blogs.data.js
@@ -1,42 +1,14 @@
-import Photo2 from "../assets/valantine.jpg";
-import Photo3 from "../assets/NEQ.png";
-import Photo4 from "../assets/Artboard6-100.jpg";
+import Photo2 from "../assets/valantine.jpg"
+import Photo3 from "../assets/NEQ.png"
+import Photo4 from "../assets/Artboard 6-100.jpg"
 
 // blogs data
 
 const blogs = [
-    
     {
         id: 1,
         title: 'Who was Saint Valentine?',
-        description: `Valentine was a Christian priest in Rome. He knew from the Bible that marriage was good and honored by God. He knew that marriage was lawful according to the Christian faith, so he took it upon himself to perform Christian marriages—contrary to the law. As a priest, he performed secret marriages for couples who desired to be married, bravely defying the anti-marriage edict. It wasn’t just marriages that Valentine was working on. He was also trying to protect persecuted Christians who were being chased down and hunted by aggressive Roman leaders. Christians knew that they could flee to Valentine to find protection. Valentine was taking a huge risk. Not only was it absolutely forbidden to marry or to perform marriages, but it was also a criminal offense to aid or abet Christians—especially ones whom the Roman Empire had on their hit list! Valentine was enmeshed in what the Roman Empire considered high treason and traitorous activity. Although he was being loyal to his faith, he was flying in the face of Roman law. The Roman government hunted him down and locked him up in prison. Now, Valentine—protector of Christians and performer of marriages—was himself suffering for his love and devotion to God.`,
-        image: Photo2,
-        url: 'https://medium.com/@dan_abramov/making-sense-of-react-hooks-fdbde8803889',
-        date: 'Thursday, 8 December 2022',
-        category: 'react'
-    },
-     {
-        id: 1,
-        title: 'Who was Saint Valentine?',
-        description: `Valentine was a Christian priest in Rome. He knew from the Bible that marriage was good and honored by God. He knew that marriage was lawful according to the Christian faith, so he took it upon himself to perform Christian marriages—contrary to the law. As a priest, he performed secret marriages for couples who desired to be married, bravely defying the anti-marriage edict. It wasn’t just marriages that Valentine was working on. He was also trying to protect persecuted Christians who were being chased down and hunted by aggressive Roman leaders. Christians knew that they could flee to Valentine to find protection. Valentine was taking a huge risk. Not only was it absolutely forbidden to marry or to perform marriages, but it was also a criminal offense to aid or abet Christians—especially ones whom the Roman Empire had on their hit list! Valentine was enmeshed in what the Roman Empire considered high treason and traitorous activity. Although he was being loyal to his faith, he was flying in the face of Roman law. The Roman government hunted him down and locked him up in prison. Now, Valentine—protector of Christians and performer of marriages—was himself suffering for his love and devotion to God.`,
-        image: Photo2,
-        url: 'https://medium.com/@dan_abramov/making-sense-of-react-hooks-fdbde8803889',
-        date: 'Thursday, 8 December 2022',
-        category: 'react'
-    },
-     {
-        id: 1,
-        title: 'Who was Saint Valentine?',
-        description: `Valentine was a Christian priest in Rome. He knew from the Bible that marriage was good and honored by God. He knew that marriage was lawful according to the Christian faith, so he took it upon himself to perform Christian marriages—contrary to the law. As a priest, he performed secret marriages for couples who desired to be married, bravely defying the anti-marriage edict. It wasn’t just marriages that Valentine was working on. He was also trying to protect persecuted Christians who were being chased down and hunted by aggressive Roman leaders. Christians knew that they could flee to Valentine to find protection. Valentine was taking a huge risk. Not only was it absolutely forbidden to marry or to perform marriages, but it was also a criminal offense to aid or abet Christians—especially ones whom the Roman Empire had on their hit list! Valentine was enmeshed in what the Roman Empire considered high treason and traitorous activity. Although he was being loyal to his faith, he was flying in the face of Roman law. The Roman government hunted him down and locked him up in prison. Now, Valentine—protector of Christians and performer of marriages—was himself suffering for his love and devotion to God.`,
-        image: Photo2,
-        url: 'https://medium.com/@dan_abramov/making-sense-of-react-hooks-fdbde8803889',
-        date: 'Thursday, 8 December 2022',
-        category: 'react'
-    },
-     {
-        id: 1,
-        title: 'Who was Saint Valentine?',
-        description: `Valentine was a Christian priest in Rome. He knew from the Bible that marriage was good and honored by God. He knew that marriage was lawful according to the Christian faith, so he took it upon himself to perform Christian marriages—contrary to the law. As a priest, he performed secret marriages for couples who desired to be married, bravely defying the anti-marriage edict. It wasn’t just marriages that Valentine was working on. He was also trying to protect persecuted Christians who were being chased down and hunted by aggressive Roman leaders. Christians knew that they could flee to Valentine to find protection. Valentine was taking a huge risk. Not only was it absolutely forbidden to marry or to perform marriages, but it was also a criminal offense to aid or abet Christians—especially ones whom the Roman Empire had on their hit list! Valentine was enmeshed in what the Roman Empire considered high treason and traitorous activity. Although he was being loyal to his faith, he was flying in the face of Roman law. The Roman government hunted him down and locked him up in prison. Now, Valentine—protector of Christians and performer of marriages—was himself suffering for his love and devotion to God.`,
+        description: "Valentine was a Christian priest in Rome. He knew from the Bible that marriage was good and honored by God. He knew that marriage was lawful according to the Christian faith, so he took it upon himself to perform Christian marriages—contrary to the law. As a priest, he performed secret marriages for couples who desired to be married bravely defying the anti-marriage edict. It wasn’t just marriages that Valentine was working on. He was also trying to protect persecuted Christians who were being chased down and haunted by the aggressive Roman leaders. Christians knew that they could flee to Valentine to find protection. Valentine was taking a huge risk. Not only was it absolutely forbidden to marry or to perform marriages, but it was also a criminal offense to aid or abet Christians—especially ones whom the Roman Empire had on their hit list! Valentine was enmeshed in what the Roman Empire considered high treason and traitorous activity. Although he was being loyal to his faith, he was flying in the face of Roman law. The Roman government hunted him down and locked him up in prison. Now, Valentine—protector of Christians and performer of marriages—was himself suffering for his love and devotion to God.",
         image: Photo2,
         url: 'https://medium.com/@dan_abramov/making-sense-of-react-hooks-fdbde8803889',
         date: 'Thursday, 8 December 2022',
@@ -44,9 +16,9 @@ const blogs = [
     },
     
     {
-       id: 3,
+       id: 2,
        title: 'SINA GERARD/Ese URWIBUTSO Is oriented towards agro-processing industry sector.',
-       description: `Our successful track record of innovation & expertise demonstrates our commitment to launching innovative products every year in order to improve our customer relationships & boost our market position. Our Mission: Processing and delivering standardized products and providing high quality services. \n\n Our Vision: Being influential through providing excellent made-in-Rwanda products; Being competitive worldwide and Enhancing social economic development. \n\nWe are located at Nyirangarama in Rulindo District, Northern Province about 45 Km from Kigali City, on Kigali Musanze – Rubavu main road.`,
+       description: 'Our successful track record of innovation & expertise demonstrates our commitment to launching innovative products every year in order to improve our customer relationships & boost our market position. Our Mission: Processing and delivering standardized products and providing high quality services. \n\n Our Vision: Being influential through providing excellent made-in-Rwanda products; Being competitive worldwide and Enhancing social economic development. \n\nWe are located at Nyirangarama in Rulindo District, Northern Province about 45 Km from Kigali City, on Kigali Musanze – Rubavu main road.',
        image: Photo4,
        url: 'https://medium.com/@dan_abramov/making-sense-of-react-hooks-fdbde8803889',
        date: 'Thursday, 9 August 2024',
@@ -54,15 +26,15 @@ const blogs = [
 
     },
     {
-        id: 4,
+        id: 3,
         title: "The Virunga Mountains and the Mysterious Mountain Gorillas",
-        description: `High up in the lush, mist-shrouded mountains of East Africa, perched on the edge of the Great Rift Valley, are a collection of volcanic peaks known as the Virunga Mountains. Up until the early 1900s, few people had ventured into these mysterious highlands straddling Rwanda, Uganda, and DRC, but explorers had heard rumours of a man-like creature living high up on the mountain slopes. In 1902, a German explorer named Captain Robert von Beringe ventured into the rainforests, returning with stories of this elusive creature that few people could believe. As proof of his encounter, he brought back the skin of one of the mountain gorillas he had found. It wasn’t until the parts had been analysed in Europe that the animal was proclaimed a new subspecies of gorilla and one that was much, much larger than any ape previously discovered. From then on, the Virunga mountains have held a powerful lure for people all around the world, and in 1950 the area became the first National Park to be proclaimed in Africa. The first concerted effort to understand the mountain gorillas was made in the 1960s, when the famous primatologist Dian Fossey began a two-decade study of the gorillas. She spent every day with the gorillas, watching and documenting their close family bonds, and provided a window into the lives of these remarkable animals. She also confronted one of the greatest threats to the gorilla’s survival—poaching for their meat and parts—a battle that eventually led to her tragic murder.`,
+        description: "High up in the lush, mist shrouded mountains of East Africa, perched on the edge of the Great Rift Valley, are a collection of volcanic peaks known as the Virunga Mountains. Up until the early 1900s, few people had ventured into these mysterious highlands straddling Rwanda, Uganda, and DRC, but explorers had heard rumours of a man-like creature living high up on the mountain slopes. In 1902, a German explorer named Captain Robert von Beringe ventured into the rainforests, returning with stories of this elusive creature that few people could believe. As proof of his encounter, he brought back the skin of one of the mountain gorillas he had found. It wasn’t until the parts had been analysed in Europe that the animal was proclaimed a new subspecies of gorilla and one that was much, much larger than any ape previously discovered. From then on, the Virunga mountains have held a powerful lure for people all around the world, and in 1950 the area became the first National Park to be proclaimed in Africa. The first concerted effort to understand the mountain gorillas was made in the 1960s, when the famous primatologist Dian Fossey began a two decade study of the gorillas. She spent every day with the gorillas, watching and documenting their close family bonds, and provided a window into the lives of these remarkable animals. She also confronted one of the greatest threats to the gorilla’s survival—poaching for their meat and parts—a battle that eventually led to her tragic murder.",
         image: Photo3,
         url: "https://medium.com/@dan_abramov/making-sense-of-react-hooks-fdbde8803889",
         date: "Thursday, 8 December 2022",
         category: "travel"
-    }
+      }
+      
       
 ];
-
 export default blogs;
